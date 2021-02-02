99e1c5001dd044f3615c2847626640a051bd2fe1
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-02-01 19:30:56 -0500

    Initial Commit, functioning mostly

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => (
+ <>
+    <h1> About</h1>
+    <p> Here</p>
+    <p>Also Here</p>
+</>
+);
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+ <>
+    <h1> Hero Message Here</h1>
+    <p> Here</p>
+    <p>Also Here</p>
+</>
+);
+
+export default HomePage;
