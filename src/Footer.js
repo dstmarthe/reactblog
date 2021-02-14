51e4842f1bb10f726284ba54cabbc5541ef5faf7
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-02-14 01:27:57 -0500

    Updates

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <p className="container-fluid p-2 text-white-50 text-center bg-secondary mb-0 fixed-bottom fs-6">
-    I own nothing.
-  </p>
-);
-
-export default Footer;
